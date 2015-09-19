8ff4a9fa0634283b6b2abfe98b34b3812a6c4d5b
Joshua Liu <joliu@htps.us>
2015-09-19 00:11:22 -0400

    Initial Commit

---
+++ MagnetPrototyping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagnetPrototyping
+//
+//  Created by Joshua Liu on 9/19/15.
+//  Copyright © 2015 Joshua Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
